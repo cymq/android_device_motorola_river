45bbfd909f3394e9d6754cdabecc2898fe239cf9
SyberHexen <SyberHexen@gmail.com>
2019-09-26 20:40:31 -0700

    river: wlan: Add wlan configs for kernel driver.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,5 +150,16 @@ BOARD_VNDK_VERSION := current
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
+# Wi-Fi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
 # Inherit from the proprietary version
 -include vendor/motorola/river/BoardConfigVendor.mk
